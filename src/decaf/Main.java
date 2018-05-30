c5b6630f29b535a5d961e8316f30e63986bd9841
diogo <diogo8-p@hotmail.com>
2018-05-30 16:08:57 -0300

    semantica 4

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.Arrays;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -118,7 +119,7 @@ class Main {
 							type = "STRINGLITERAL";
 							break;
 						case DecafLexer.HEX_CEPTION:
-							throw new Exception("erritcho da lulu");
+							throw new Exception("erro");
 
 		        			}
 		        			System.out.println (token.getLine() + type + " " + text);
@@ -150,17 +151,21 @@ class Main {
                     //show AST in console
                     System.out.println(tree.toStringTree(parser));
 
-                    //show AST in GUI
-                    JFrame frame = new JFrame("Antlr AST");
-                    JPanel panel = new JPanel();
-                    TreeViewer viewr = new TreeViewer(Arrays.asList(
-                            parser.getRuleNames()),tree);
-                    viewr.setScale(1.5);//scale a little
-                    panel.add(viewr);
-                    frame.add(panel);
-                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    frame.setSize(800,500);
-                    frame.setVisible(true);
+		      //show AST in GUI
+				JFrame frame = new JFrame("Antlr AST");
+				JPanel panel = new JPanel();
+	    JScrollPane scrollPane = new JScrollPane(panel);
+	    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+	    scrollPane.setBounds(50, 30, 300, 50);
+				TreeViewer viewr = new TreeViewer(Arrays.asList(
+						parser.getRuleNames()),tree);
+				viewr.setScale(1.5);//scale a little
+				panel.add(viewr);
+				frame.add(scrollPane);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(600,400);
+				frame.setVisible(true);
                 }
 
             } else if (CLI.target == CLI.INTER) {
